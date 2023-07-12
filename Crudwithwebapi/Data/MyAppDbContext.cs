182a84ddcc44c3ed51db61cc066c0a32c86d3968
karanpatel4882 <karanpatel85300@gmail.com>
2023-07-12 10:40:03 +0530

    Add project files.

---
+++ Crudwithwebapi/Data/MyAppDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+using Crudwithwebapi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crudwithwebapi.Data
+{
+    public class MyAppDbContext:DbContext
+    {
+        public MyAppDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
+                     
